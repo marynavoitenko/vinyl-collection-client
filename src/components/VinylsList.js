1a1c174ebafb904414b0438bddac29bfedf95d2c
marynavoitenko <maryna.voitenko@gmail.com>
2018-12-24 16:44:01 -0500

    Initial routing setup

--- src/components/VinylsList.js
+++ src/components/VinylsList.js
@@ -3,7 +3,7 @@ import Vinyl from './Vinyl';
 
 const VinylsList = ({ vinyls }) => {
   const renderVinyls = vinyls.map((vinyl) =>
-    <Vinyl vinyl={vinyl} />
+    <Vinyl key={vinyl.id} vinyl={vinyl} />
   );
 
   return (
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import VinylsCollectionPage from './VinylsCollectionPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <VinylsCollectionPage />
-      </div>
+      <Router>
+        <Route path="/" component={VinylsCollectionPage} />
+      </Router>
     );
   }
 }
