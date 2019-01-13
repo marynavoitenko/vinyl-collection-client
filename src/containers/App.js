a38321693279fcc67c8a64fc059aa407bb0220c3
marynavoitenko <maryna.voitenko@gmail.com>
2019-01-12 19:07:34 -0500

    Add navbar and background styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import VinylsPage from './VinylsPage';
 import ArtistsPage from './ArtistsPage';
+import Home from '../components/Home';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Fragment>
-          <NavBar />
-          <Route exact path='/' component={VinylsPage} />
-          <Route path='/vinyls' component={VinylsPage} />
-          <Route path='/artists' component={ArtistsPage} />
+          <div className='container'>
+            <NavBar />
+            <Route exact path='/' component={Home} />
+            <Route path='/vinyls' component={VinylsPage} />
+            <Route path='/artists' component={ArtistsPage} />
+          </div>
         </Fragment>
       </Router>
     );
